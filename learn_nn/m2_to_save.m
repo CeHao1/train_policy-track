c48bbe6079043d0c4ce31f713e3744519fd5eb09
Ce Hao <57501775+CeHao1@users.noreply.github.com>
2021-01-11 16:32:22 +0800

    new

--- learn_nn/m2_to_save.m
+++ learn_nn/m2_to_save.m
@@ -3,9 +3,9 @@
 
 
 %% 
-train_data=[policies,Fs];
+data=[policies,Fs];
 
-train_targets=[];
+targets=[];
 num_intp=100;
 
 for i =1:num_pts
@@ -14,11 +14,11 @@ for i =1:num_pts
     x1=linspace(-F1,0,num_intp);
     x2=linspace(0,F2,num_intp);
     x3=linspace(F2,1,num_intp);
-    train_targets(i,:)=spline(r{i,1}.X,r{i,1}.Y,[x1,x2,x3]);
+    targets(i,:)=spline(r{i,1}.X,r{i,1}.Y,[x1,x2,x3]);
     
 end
 
 % save('train_data','train_data')
 % save('train_targets','train_targets')
-save('train_both2','train_data','train_targets')
+save('train_both','data','targets')
 
