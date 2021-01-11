4e829a8226ae6c77adc91f945fe82a4188b8f67d
Ce Hao <57501775+CeHao1@users.noreply.github.com>
2021-01-11 15:26:44 +0800

    1

--- learn_nn/initial.m
+++ learn_nn/initial.m
@@ -2,7 +2,7 @@ clc;clear
 close all
 
 
-num_pts=1000;
+num_pts=100;
 num_policy=20;
 num_F=20;
 a=round(rand(num_pts,1)*num_policy^4*num_F^2);
@@ -12,9 +12,9 @@ a=round(rand(num_pts,1)*num_policy^4*num_F^2);
 sz=[num_F,num_F,num_policy,num_policy,num_policy,num_policy];
 [p(1,:),p(2,:),p(3,:),p(4,:),f(1,:),f(2,:)]=ind2sub(sz,a);
 
-
+bound=0.1;
 policy_base=linspace(-1,1,num_policy);
-F_base=linspace(1e-2,1-1e-2,num_F);
+F_base=linspace(bound,1-bound,num_F);
 
 policies=policy_base(p)';
 Fs=F_base(f)';
--- learn_nn/test1.m
+++ learn_nn/test1.m
@@ -1,13 +1,23 @@
-clc;clear
-close all
-
-p=parpool();
-data=cell(1);
+% clc;clear
+% close all
 
 
-parfor i=1:5
-    a=i+2;
-    data{i,1}=a;
-end
+policy=[0,-0.6,0.6,-0.6];
+F=[0.3,0.5];
 
-delete(p)
+[track,path]=construct_normal_path(policy,F);
+
+
+%%
+
+figure
+hold on
+plot(track.Center.X,track.Center.Y,'b')
+plot(track.Left.X,track.Left.Y,'b')
+plot(track.Right.X,track.Right.Y,'r')
+
+plot(path.X,path.Y,'k','linewidth',2)
+plot(path.PX,path.PY,'ro')
+
+hold off
+axis equal
