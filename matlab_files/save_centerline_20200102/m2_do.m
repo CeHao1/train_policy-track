28c97e5e4ddcfebec8407a1c7160291ed1a2387f
Ce Hao <57501775+CeHao1@users.noreply.github.com>
2021-02-01 08:55:31 +0800

    1

--- matlab_files/save_centerline_20200102/m2_do.m
+++
@@ -1,7 +0,0 @@
-
-close all
-waypoint.Center=make_smooth(waypoint.Center,0.4,1e-7);
-waypoint.Left=make_smooth(waypoint.Left,0.4,1e-7);
-waypoint.Right=make_smooth(waypoint.Right,0.4,1e-7);
-
-save('Tokyo','waypoint');
